40c4850c47ad2bc916aac27aa537a0a638ca2a1e
pointfour <lesha.seletskiy@gmail.com>
2019-05-11 08:30:49 -0700

    data

--- server/main.go
+++ server/main.go
@@ -4,40 +4,54 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"math/rand"
 	"net/http"
-	"time"
+	"strings"
 )
 
-const SIZE int = 5
+const SIZE int = 2
 
 type stateMatrix struct {
-	state map[[SIZE]byte]map[byte]float64
+	Trained map[[SIZE]string]strAndProb
 }
+
+type strAndProb struct {
+	Str  string
+	Prob float64
+}
+
+type trainingMatrix map[[SIZE]string]map[string]float64
 
 var model stateMatrix
 
 func main() {
-	rand.Seed(time.Now().UTC().UnixNano())
-	input := loadText("source.txt")
-	model = stateMatrix{state: make(map[[SIZE]byte]map[byte]float64)}
-	train(model.state, input)
-	// fmt.Println(model.generate())
-	// ioutil.WriteFile("out.txt", []byte(model.generate()), 0644)
+	// rand.Seed(time.Now().UTC().UnixNano())
+	input := loadText("source/news.txt")
+	// for i := 0; i < len(input); i++ {
+	// 	fmt.Println(input[i])
+	// }
+	train(&model, input)
+	fmt.Println(generate(&model, [SIZE]string{"implied", "your"}))
 	http.HandleFunc("/", requestHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
 	enableCors(&w)
-	fmt.Println(r.RequestURI)
-	querybytes := []byte(r.RequestURI)
-	var query []byte
-	for i := 0; i < SIZE; i++ {
-		query = append(query, querybytes[i+1])
+	processedURI := strings.Replace(r.RequestURI[1:], "%20", " ", -1)
+	fmt.Println(processedURI)
+
+	splitString := strings.Fields(processedURI)
+	input := [SIZE]string{}
+	if len(splitString) < SIZE {
+		fmt.Println("invalid request")
+		return
 	}
-	strinky := model.generate(string(query))
-	fmt.Fprintf(w, strinky)
+	for i := 0; i < SIZE; i++ {
+		input[i] = splitString[i]
+	}
+	output := generate(&model, input)
+	fmt.Println(output)
+	fmt.Fprintf(w, strings.Join(output, " "))
 }
 
 func enableCors(w *http.ResponseWriter) {
@@ -45,75 +59,75 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
 }
 
-func (m stateMatrix) generate(inputStr string) string {
-	nxt := []byte(inputStr)
-	seed := [SIZE]byte{} //{69, 109}
-	for i := 0; i < SIZE; i++ {
-		seed[i] = nxt[i]
-	}
-	ret := []byte{}
-	// for i := 0; i < SIZE; i++ {
-	// 	ret = append(ret, seed[i])
-	// }
-
-	var done bool
-	for i := 0; i < 10000-SIZE; i++ {
-		getTo := rand.Float64()
-		var at float64
-		if done {
+func generate(model *stateMatrix, input [SIZE]string) []string {
+	ret := []string{}
+	key := input
+	var probability float64 = 1
+	for true {
+		if len(ret) > 6 {
 			break
 		}
-		for key, value := range m.state[seed] {
-			at += value
-			if at > getTo {
-				if key == 32 || key == 10 {
-					done = true
-					break
+		if _, exists := model.Trained[key]; exists {
+			probability *= model.Trained[key].Prob
+			fmt.Println(model.Trained[key])
+
+			if probability > 0.1 {
+				ret = append(ret, model.Trained[key].Str)
+				last := model.Trained[key].Str
+				for i := 0; i < len(key)-1; i++ {
+					key[i] = key[i+1]
 				}
-				ret = append(ret, key)
-				// seed = [SIZE]byte{seed[1], seed[2], key}
-				cseed := [SIZE]byte{}
-				for s := 1; s < SIZE; s++ {
-					cseed[s-1] = seed[s]
-				}
-				cseed[SIZE-1] = key
-				seed = cseed
+				key[len(key)-1] = last
+			} else {
 				break
 			}
+		} else {
+			break
 		}
 	}
-	return string(ret)
+	return ret
 }
 
-func train(state map[[SIZE]byte]map[byte]float64, input []byte) {
-	norm := make(map[[SIZE]byte]float64)
+func train(model *stateMatrix, input []string) {
+	matrix := make(trainingMatrix)
 	for i := 0; i < len(input)-SIZE; i++ {
-		key := [SIZE]byte{}
+		key := [SIZE]string{}
 		for x := 0; x < SIZE; x++ {
 			key[x] = input[i+x]
 		}
 		val := input[i+SIZE]
-		if state[key] == nil {
-			state[key] = make(map[byte]float64)
+		if matrix[key] == nil {
+			matrix[key] = make(map[string]float64)
 		}
-		if state[key][val] == 0 {
-			state[key][val] = 1
-			norm[key] = 1
+		if matrix[key][val] == 0 {
+			matrix[key][val] = 1
 		} else {
-			state[key][val]++
-			norm[key]++
-		}
-		// fmt.Println(state[key][val])
-	}
-	for key := range state {
-		for skey := range state[key] {
-			state[key][skey] /= norm[key]
+			matrix[key][val]++
 		}
 	}
-
+	model.Trained = make(map[[SIZE]string]strAndProb)
+	for key := range matrix {
+		var sum float64
+		var biggestSkey string
+		var biggestVal float64
+		for skey := range matrix[key] {
+			sum += matrix[key][skey]
+			if matrix[key][skey] > biggestVal {
+				biggestVal = matrix[key][skey]
+				biggestSkey = skey
+			}
+		}
+		model.Trained[key] = strAndProb{biggestSkey, biggestVal / sum}
+		// fmt.Println(key, sum, biggestVal, biggestSkey)
+	}
+	// fmt.Println(model.Trained)
 }
 
-func loadText(path string) []byte {
-	ret, _ := ioutil.ReadFile(path)
+func loadText(path string) []string {
+	unProcessed, _ := ioutil.ReadFile(path)
+	ret := strings.Fields(string(unProcessed))
+	for i := 0; i < len(ret); i++ {
+		ret[i] = strings.ToLower(ret[i])
+	}
 	return ret
 }
