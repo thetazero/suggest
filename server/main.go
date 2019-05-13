66e74d9c262f7b8440f63e8038eb23991f129006
pointfour <lesha.seletskiy@gmail.com>
2019-05-13 11:21:13 -0700

    memory optimizations

--- server/main.go
+++ server/main.go
@@ -11,28 +11,46 @@ import (
 const SIZE int = 2
 
 type stateMatrix struct {
-	Trained map[[SIZE]string]strAndProb
+	Trained map[[SIZE]int]strAndProb
+	Count   int
+	Encoder map[string]int
+	Decoder map[int]string
 }
 
 type strAndProb struct {
-	Str  string
+	Str  int
 	Prob float64
 }
 
-type trainingMatrix map[[SIZE]string]map[string]float64
+type trainingMatrix map[[SIZE]int]map[int]float64
 
 var model stateMatrix
 
 func main() {
 	// rand.Seed(time.Now().UTC().UnixNano())
-	input := loadText("source/news.txt")
+	input := loadText("source/myemails.txt")
 	// for i := 0; i < len(input); i++ {
 	// 	fmt.Println(input[i])
 	// }
 	train(&model, input)
-	fmt.Println(generate(&model, [SIZE]string{"implied", "your"}))
+	fmt.Println("trained")
+	// fmt.Println(generate(&model, [SIZE]int{encode("implied"), encode("your")}))
 	http.HandleFunc("/", requestHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func encode(s string) int {
+	if model.Encoder[s] == 0 {
+		model.Count++
+		model.Encoder[s] = model.Count
+		model.Decoder[model.Count] = s
+		return model.Count
+	}
+	return model.Encoder[s]
+}
+
+func decode(num int) string {
+	return model.Decoder[num]
 }
 
 func requestHandler(w http.ResponseWriter, r *http.Request) {
@@ -41,17 +59,21 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(processedURI)
 
 	splitString := strings.Fields(processedURI)
-	input := [SIZE]string{}
+	input := [SIZE]int{}
 	if len(splitString) < SIZE {
 		fmt.Println("invalid request")
 		return
 	}
 	for i := 0; i < SIZE; i++ {
-		input[i] = splitString[i]
+		input[i] = encode(splitString[i])
 	}
 	output := generate(&model, input)
 	fmt.Println(output)
-	fmt.Fprintf(w, strings.Join(output, " "))
+	strOutput := []string{}
+	for i := 0; i < len(output); i++ {
+		strOutput = append(strOutput, decode(output[i]))
+	}
+	fmt.Fprintf(w, strings.Join(strOutput, " "))
 }
 
 func enableCors(w *http.ResponseWriter) {
@@ -59,8 +81,8 @@ func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Methods", "*")
 }
 
-func generate(model *stateMatrix, input [SIZE]string) []string {
-	ret := []string{}
+func generate(model *stateMatrix, input [SIZE]int) []int {
+	ret := []int{}
 	key := input
 	var probability float64 = 1
 	for true {
@@ -89,15 +111,17 @@ func generate(model *stateMatrix, input [SIZE]string) []string {
 }
 
 func train(model *stateMatrix, input []string) {
+	model.Encoder = make(map[string]int)
+	model.Decoder = make(map[int]string)
 	matrix := make(trainingMatrix)
 	for i := 0; i < len(input)-SIZE; i++ {
-		key := [SIZE]string{}
+		key := [SIZE]int{}
 		for x := 0; x < SIZE; x++ {
-			key[x] = input[i+x]
+			key[x] = encode(input[i+x])
 		}
-		val := input[i+SIZE]
+		val := encode(input[i+SIZE])
 		if matrix[key] == nil {
-			matrix[key] = make(map[string]float64)
+			matrix[key] = make(map[int]float64)
 		}
 		if matrix[key][val] == 0 {
 			matrix[key][val] = 1
@@ -105,10 +129,10 @@ func train(model *stateMatrix, input []string) {
 			matrix[key][val]++
 		}
 	}
-	model.Trained = make(map[[SIZE]string]strAndProb)
+	model.Trained = make(map[[SIZE]int]strAndProb)
 	for key := range matrix {
 		var sum float64
-		var biggestSkey string
+		var biggestSkey int
 		var biggestVal float64
 		for skey := range matrix[key] {
 			sum += matrix[key][skey]
@@ -117,7 +141,9 @@ func train(model *stateMatrix, input []string) {
 				biggestSkey = skey
 			}
 		}
-		model.Trained[key] = strAndProb{biggestSkey, biggestVal / sum}
+		if biggestVal > 0 {
+			model.Trained[key] = strAndProb{biggestSkey, biggestVal / sum}
+		}
 		// fmt.Println(key, sum, biggestVal, biggestSkey)
 	}
 	// fmt.Println(model.Trained)
